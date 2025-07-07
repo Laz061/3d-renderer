8455b03884f05e8c545ff776cb1dc23c41cbfae4
Laz061 <yhua536@aucklanduni.ac.nz>
2025-07-07 17:10:26 +1200

    feat: add z buffer to render the correct pixel when faces overlap

--- src/main/java/com/github/laz061/renderer3d/main.java
+++ src/main/java/com/github/laz061/renderer3d/main.java
@@ -2,7 +2,6 @@
 package com.github.laz061.renderer3d;
 
 import java.awt.*;
-import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.*;
@@ -63,10 +62,16 @@ public class Main {
                         0, Math.cos(pitchAngle), Math.sin(pitchAngle),
                         0, -Math.sin(pitchAngle), Math.cos(pitchAngle)
                 });
-
                 Matrix totalTransform = yawTransform.multiply(pitchTransform);
 
                 BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                // z buffer to properly render faces that overlap
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                // initialize array with extremely far away depths
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
 
                 // draws four triangles forming tetrahedral
                 for (Triangle t : tris) {
@@ -74,7 +79,6 @@ public class Main {
                     Vertex v1 = totalTransform.transform(t.v1);
                     Vertex v2 = totalTransform.transform(t.v2);
                     Vertex v3 = totalTransform.transform(t.v3);
-                    // we have to do translation manually
                     v1.x += getWidth() / 2;
                     v1.y += getHeight() / 2;
                     v2.x += getWidth() / 2;
@@ -90,7 +94,7 @@ public class Main {
                     int maxY = (int) Math.min(img.getHeight() - 1,
                             Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
 
-                    // calculates the parallellogram area using v3->v1 and v3->v2
+                    // calculates the parallelogram area using v3->v1 and v3->v2
                     double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) - (v1.x - v3.x) * (v2.y - v3.y);
 
                     // loops through all pixels in the rectangle encapsulating the triangle
@@ -104,15 +108,23 @@ public class Main {
 
                             // v2 -> p and v2 -> v1
                             double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+                            // check if pixel in triangle
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                                img.setRGB(x, y, t.color.getRGB());
+                                // barycentric interpolation for the depth of pixel
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                // only paint pixel if it is the closer than the previous pixel
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
                             }
                         }
                     }
                 }
 
                 g2.drawImage(img, 0, 0, null);
-
             }
         };
 

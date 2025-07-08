70bbc02f21de96c035e6f14bcaf0f792c73140f5
Laz061 <yhua536@aucklanduni.ac.nz>
2025-07-09 00:15:52 +1200

    feat: add sphere model and button - created a shapeFactory.java class to handle the creation of shape lists - created the sphere model using triangle subdivision of the tetrahedron

--- src/main/java/com/github/laz061/renderer3d/main.java
+++ src/main/java/com/github/laz061/renderer3d/main.java
@@ -4,8 +4,6 @@ import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.FlatDarkLaf;
 import java.awt.*;
 import javax.swing.*;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,47 +27,33 @@ public class Main {
         RenderPanel renderPanel = new RenderPanel(yawSlider, pitchSlider);
         pane.add(renderPanel, BorderLayout.CENTER);
 
+        yawSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
+
         // Control panel
         JPanel controlPanel = new JPanel();
-        controlPanel.setLayout(new BorderLayout());
+        controlPanel.setLayout(new FlowLayout());
         controlPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         JButton tetrahedronButton = new JButton("Tetrahedron");
         controlPanel.add(tetrahedronButton);
 
         tetrahedronButton.addActionListener(e -> {
-            renderPanel.setShape(createTetrahedron());
+            renderPanel.setShape(ShapeFactory.tetrahedron());
+            renderPanel.repaint();
+        });
+
+        JButton sphereButton = new JButton("Sphere");
+        controlPanel.add(sphereButton);
+
+        sphereButton.addActionListener(e -> {
+            renderPanel.setShape(ShapeFactory.sphere());
             renderPanel.repaint();
         });
 
         pane.add(controlPanel, BorderLayout.WEST);
 
-        yawSlider.addChangeListener(e -> renderPanel.repaint());
-        pitchSlider.addChangeListener(e -> renderPanel.repaint());
-
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private static List<Triangle> createTetrahedron() {
-        List<Triangle> tris = new ArrayList<>();
-        tris.add(new Triangle(new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
-                new Vertex(-100, 100, -100),
-                Color.WHITE));
-        tris.add(new Triangle(new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
-                new Vertex(100, -100, -100),
-                Color.RED));
-        tris.add(new Triangle(new Vertex(-100, 100, -100),
-                new Vertex(100, -100, -100),
-                new Vertex(100, 100, 100),
-                Color.GREEN));
-        tris.add(new Triangle(new Vertex(-100, 100, -100),
-                new Vertex(100, -100, -100),
-                new Vertex(-100, -100, 100),
-                Color.BLUE));
-
-        return tris;
     }
 }
